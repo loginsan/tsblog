53e7c7c59c2b51d294920e5cbc38f142e2254614
Ilya Loginov <linsykt@gmail.com>
2021-03-30 01:52:12 +0300

    add 75% form styles and markup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,52 @@
-import React from "react";
-import { BrowserRouter as Router, Link, NavLink } from "react-router-dom";
-import "./App.scss";
-import profileAvatar from "./user-avatar.png";
+import React, { useState, useCallback } from 'react';
+import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
+import './App.scss';
+import profileAvatar from './user-avatar.png';
+
 
 function App() {
+  const [fields, setFields] = useState({
+    username: '',
+    email: '',
+    password: '',
+    repeatpassword: ''
+  });
+
+  const [article, setArticle] = useState({
+    title: '',
+    short: '',
+    text: '',
+    tags: []
+  });
+
+  const handleChange = useCallback(
+    (evt: React.FormEvent<HTMLInputElement>, field: string) => {
+    // console.log(evt);
+    const val: string = evt.currentTarget.value;
+    setFields( { [field]: val, ...fields } );
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1 className="AppTitle"><NavLink to="/">Realworld Blog</NavLink></h1>
+          <h1 className="AppTitle">
+            <NavLink to="/">Realworld Blog</NavLink>
+          </h1>
           <div className="UserMenu">
-            <Link to="/sign-in" className="SignInLink">Sign In</Link>
-            <Link to="/sign-up" className="SignUpLink">Sign Up</Link>
-            <Link to="/new-article" className="NewArticleLink" title="Create new article">Create article</Link>
-            <Link to="/profile" className="EditProfileLink" title="Edit profile">John Doe <img src={profileAvatar} alt="Avatar" className="Avatar" /></Link>
-            <Link to="/logout" className="LogOutLink">Log Out</Link>
+            <Link to="/sign-in" className="SignInLink">
+              Sign In
+            </Link>
+            <Link to="/sign-up" className="SignUpLink">
+              Sign Up
+            </Link>
           </div>
         </header>
+
         <main className="AppMain">
-          <section className="PageContent">
-            Page Content
-          </section>
-          <section className="FormContent">
-            Form Content
-          </section>
+          <section className="PageContent">Page Content</section>
+          <section className="PageContent">Page Content</section>
+          
         </main>
       </div>
     </Router>
